778d750bfae2eecd56e776b43c594491ecb678b1
Ilya Kuznetsov <kromsik@icloud.com>
2021-03-04 20:17:43 +0300

    task 1

--- Geekbrains/main.swift
+++ Geekbrains/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+// Решить квадратное уравнение
+
+let a: Float = 1
+let b: Float = -2
+let c: Float = -3
+
+let d = pow(b, 2) - 4 * a * c
+
+if (d < 0) {
+    print("Корней нет")
+} else if (d == 0) {
+    print("1 решение")
+    let x = (-b - sqrt(d)) / (2*a)
+    print(x)
+} else {
+    print("2 решения")
+    let x1 = (-b - sqrt(d)) / (2*a)
+    let x2 = (-b + sqrt(d)) / (2*a)
+    print(x1, x2)
+}
 
